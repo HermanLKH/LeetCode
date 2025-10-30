04bf882d60cc3474caa2a119675d19563dfd01b2
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2025-10-31 02:05:25 +0800

    Time: 1786 ms (5.02%), Space: 74.1 MB (92.92%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // if there is only one number -> false
+        if (nums.size() == 1) {
+            return false;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j <= i + k && j < nums.size(); ++j) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
