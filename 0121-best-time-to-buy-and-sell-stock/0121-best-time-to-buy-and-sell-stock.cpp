18e24c2c6f6210e1c284b4d1b6f666fc821a53af
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2025-11-01 06:37:35 +0800

    Time: 0 ms (100%), Space: 97.2 MB (98.98%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,20 +3,14 @@ public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
         int minPrice = prices[0];
-        int maxPrice;
 
         for (int price: prices) {
-            // if yield better profit, update max price to sell
-            if (price - minPrice > profit) {
-                maxPrice = price;
-                // calculate profit
-                profit = maxPrice - minPrice;
-            }
+            // calculate profit
+            profit = max(price - minPrice, profit); 
+
             // update current price for min price for next iteration only 
             // (last price wont be sell price)
-            if (price < minPrice) {
-                minPrice = price;
-            }
+            minPrice = min(price, minPrice);
         }
         return profit;
     }
