c51fda0cc0d7c3d9df664c12f111b06149227117
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-08-29 09:32:46 +0800

    Time: 363 ms (43.21%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -6,6 +6,5 @@ FROM Prices p
 LEFT JOIN UnitsSold u
 ON
     u.product_id = p.product_id AND
-    u.purchase_date >= p.start_date AND
-    u.purchase_date <= p.end_date
+    u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id
