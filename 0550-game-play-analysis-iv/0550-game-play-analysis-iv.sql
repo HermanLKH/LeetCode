c3670cf442269183db1545407d24f553d5338031
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-01 20:46:19 +0800

    Time: 1616 ms (18.82%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,38 @@
+-- /* Write your T-SQL query statement below */
+SELECT
+    ROUND(AVG(player_fraction), 2) AS fraction
+FROM
+(
+    SELECT
+        player_id, 
+        SUM(
+            CASE
+                WHEN date_diff = 1
+                    THEN 1.0
+                ELSE 0
+            END
+        ) AS player_fraction
+    FROM
+    (
+        SELECT
+            player_id,
+            -- event_date,
+            LAG(event_date, 1, NULL) OVER(PARTITION BY player_id ORDER BY event_date) prev_date,
+            DATEDIFF(DAY, LAG(event_date, 1, NULL) OVER(PARTITION BY player_id ORDER BY event_date), event_date) date_diff
+        FROM 
+        (
+            SELECT
+                player_id,
+                event_date,
+                RANK() OVER(PARTITION BY player_id ORDER BY event_date) rank
+            FROM Activity
+        )t1
+        WHERE rank <= 2
+    )t2
+    GROUP BY player_id
+)t3
+
+-- SELECT
+--     MIN(event_date)
+-- FROM Activity
+-- GROUP BY player_id
