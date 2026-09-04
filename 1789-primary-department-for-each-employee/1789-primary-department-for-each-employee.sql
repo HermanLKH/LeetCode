681bab49b4299c753de9f0d245bd50d224dc8ae1
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-04 12:19:22 +0800

    Time: 440 ms (81.19%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,20 +1,14 @@
 /* Write your T-SQL query statement below */
-WITH CTE_Employee_Without_Department AS
+WITH CTE_Employee_Department AS
 (
     SELECT
         employee_id,
-        CASE 
-            WHEN SUM(CASE WHEN primary_flag = 'Y' THEN 1 ELSE 0 END) = 0
-                THEN 1
-            ELSE 0
-        END AS no_primary_dept
+        department_id,
+        RANK() OVER(PARTITION BY employee_id ORDER BY primary_flag DESC) rank
     FROM Employee
-    GROUP BY employee_id 
 )
 SELECT
     employee_id,
     department_id
-FROM Employee
-WHERE
-    primary_flag = 'Y' OR
-    employee_id IN (SELECT employee_id FROM CTE_Employee_Without_Department WHERE no_primary_dept = 1)
+FROM CTE_Employee_Department
+WHERE rank = 1
