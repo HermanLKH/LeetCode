54f6629bdbdc2b5f4aef9abc89a37c8ce4535de3
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-08 11:40:10 +0800

    Time: 1355 ms (7.95%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,23 @@
+/* Write your T-SQL query statement below */
+WITH CTE_Products_Price_By_Date AS
+(
+    SELECT
+        product_id,
+        new_price,
+        change_date,
+        RANK() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS date_rank
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
+
+SELECT
+    product_id,
+    new_price AS price
+FROM CTE_Products_Price_By_Date
+WHERE date_rank = 1
+UNION
+SELECT
+    product_id,
+    10 AS price
+FROM Products
+WHERE product_id NOT IN (SELECT DISTINCT product_id FROM CTE_Products_Price_By_Date)
