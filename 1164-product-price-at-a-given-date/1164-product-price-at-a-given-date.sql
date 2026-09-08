1779062007f3569dfd7a983754ebf64a395272ac
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-08 11:56:36 +0800

    Time: 765 ms (98.18%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,23 +1,18 @@
 /* Write your T-SQL query statement below */
-WITH CTE_Products_Price_By_Date AS
+WITH CTE_Ranked_Products_Price AS
 (
     SELECT
         product_id,
         new_price,
         change_date,
-        RANK() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS date_rank
+        RANK() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS rn
     FROM Products
     WHERE change_date <= '2019-08-16'
 )
-
 SELECT
-    product_id,
-    new_price AS price
-FROM CTE_Products_Price_By_Date
-WHERE date_rank = 1
-UNION
-SELECT
-    product_id,
-    10 AS price
-FROM Products
-WHERE product_id NOT IN (SELECT product_id FROM CTE_Products_Price_By_Date)
+    p.product_id,
+    COALESCE(crp.new_price, 10) AS price
+FROM (SELECT DISTINCT product_id FROM Products) AS p
+LEFT JOIN CTE_Ranked_Products_Price AS crp
+    ON p.product_id = crp.product_id
+    AND crp.rn = 1;
