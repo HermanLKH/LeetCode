e4079234c6f5fe78627afd82786435bfc1563287
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-11 11:42:08 +0800

    Time: 2296 ms (5.02%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -4,25 +4,13 @@ SELECT TOP(1)
     SUM(num) AS num
 FROM (
     SELECT
-        COUNT(
-            CASE
-                WHEN accept_date IS NOT NULL
-                    THEN 1
-                ELSE 0
-            END
-        ) AS num,
+        COUNT(accept_date) AS num,
         accepter_id
     FROM RequestAccepted
     GROUP BY accepter_id
     UNION ALL
     SELECT
-        COUNT(
-            CASE
-                WHEN accept_date IS NOT NULL
-                    THEN 1
-                ELSE 0
-            END
-        ) AS num,
+        COUNT(accept_date) AS num,
         requester_id
     FROM RequestAccepted
     GROUP BY requester_id
