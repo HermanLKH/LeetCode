c1cb8183d3efc9bb850815337f7b272778baac43
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-12 11:45:56 +0800

    Time: 497 ms (54.3%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,15 +1,21 @@
 /* Write your T-SQL query statement below */
+WITH CTE_RankedEmployees AS
+(
+    SELECT
+        name,
+        salary,
+        departmentId,
+        DENSE_RANK() OVER (
+            PARTITION BY departmentId
+            ORDER BY salary DESC
+        ) AS salary_rank
+    FROM Employee
+)
 SELECT
     d.name AS Department,
-    e.name AS Employee,
-    e.salary AS Salary
-FROM Employee e
-LEFT JOIN Department d
-ON d.id = e.departmentId
-WHERE e.salary IN
-(
-    SELECT DISTINCT TOP(3) salary
-    FROM Employee
-    WHERE departmentId = e.departmentId
-    ORDER BY salary DESC
-)
+    cre.name AS Employee,
+    cre.salary AS Salary
+FROM CTE_RankedEmployees cre
+INNER JOIN Department d
+    ON d.id = cre.departmentId
+WHERE cre.salary_rank <= 3;
