8ca3d6c2a1ece090dc68972299b6b983ac790f06
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-14 08:49:28 +0800

    Time: 0 ms (100%), Space: 19.3 MB (19.62%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,17 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        if len(digits) == 0:
-            return None
-
         digits[-1] += 1
 
         for i in range(len(digits) - 1, -1, -1):
             if digits[i] == 10:
                 digits[i] = 0
 
-                if (i - 1) == -1:
-                    digits.insert(0, 1)
-                    return digits
+                if i == 0:
+                    return [1] + digits
                 else:
                     digits[i - 1] += 1
             else:
