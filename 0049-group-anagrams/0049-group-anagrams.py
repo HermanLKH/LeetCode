152424316acbf5c4907e7726cf187ad0f79cab91
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-14 15:57:04 +0800

    Time: 67 ms (5.12%), Space: 22.1 MB (53.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,30 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        all_empty = all(s == "" for s in strs)
+        ans = defaultdict(list)
 
-        if all_empty:
-            return [strs]
+        for s in strs:
+            key = "".join(sorted(s))
+            ans[key].append(s)
         
-        patterns, groups, counts = {}, [], {}
-
-        for i, s in enumerate(strs):
-            patterns[s] = {}
-            counts[s] = counts.get(s, 0) + 1
-
-            for c in s:
-                patterns[s][c] = patterns[s].get(c, 0) + 1
-        
-        grouped_str = {}
-
-        for s, str_pattern in patterns.items():
-            for s2, str_pattern2 in patterns.items():
-                if str_pattern == str_pattern2 and not grouped_str.get(s2, False):
-                    if s == s2:
-                        groups.append([])
-
-                    for _ in range(counts.get(s2, 1)):
-                        groups[-1].append(s2)
-                        
-                    grouped_str[s2] = True
-
-        return groups        
+        return list(ans.values())
