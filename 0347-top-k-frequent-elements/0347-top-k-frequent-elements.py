6948edcce59507cda00c60aef4f122a165bd3b97
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-15 12:00:35 +0800

    Time: 7 ms (52.31%), Space: 22.9 MB (60.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_nums = {}
+        if len(nums) == k:
+            return nums
 
-        for num in nums:
-            count_nums[num] = count_nums.get(num, 0) + 1
+        count_nums = Counter(nums)
 
-        sorted_count_nums = dict(sorted(count_nums.items(), key=lambda item: item[1], reverse=True))
-        ans = []
-
-        for num, count in sorted_count_nums.items():
-            if k > 0:
-                ans.append(num)
-                k -= 1
-        
-        return ans
+        return heapq.nlargest(k, count_nums.keys(), key=count_nums.get)
