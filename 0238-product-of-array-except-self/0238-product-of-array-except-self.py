d8745cd12ecd519c809daa4b784f77c316cbbaf3
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-15 13:55:03 +0800

    Time: 15 ms (92.34%), Space: 25.1 MB (89.47%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,29 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        total_products = 1
+        has_zero = False
+        has_zeroes = False
+
+        for num in nums:
+            if num != 0:
+                total_products *= num
+            else:
+                if not has_zero:
+                    has_zero = True 
+                else:
+                    has_zeroes = True  
+
+        ans = []
+
+        for num in nums:
+            if has_zeroes:
+                ans.append(0)
+            elif has_zero:
+                if num == 0:
+                    ans.append(int(total_products))
+                else:
+                    ans.append(0)
+            else:
+                ans.append(int(total_products / num))
+                
+        return ans
